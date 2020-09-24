b6a239ea30220fa59405d65830fe40dca119e446
witheredstar <44917928+witheredstar@users.noreply.github.com>
2020-09-24 22:40:01 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -25,9 +25,9 @@ def start():
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         #print(time)
-        #if sever == 'on':
-            #requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
-    #else:
+        if sever == 'on':
+            requests.get('https://sc.ftqq.com/' + '.send?text='+mess+'，you have '+time+' days left')
+    else:
         requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
 
 def main_handler(event, context):
